411c03ad300a956ede35362137ef3a27956fbe7f
Henry Okeke <iamhenryokeke@gmail.com>
2023-01-26 22:29:39 +0100

    updated test for caeser cipher

--- caesarCipher/caeserCipher.test.js
+++ caesarCipher/caeserCipher.test.js
@@ -1,25 +1,13 @@
 const caesarCipher = require("./caeserCipher");
 
-describe("Caesar Cipher", () => {
-  test("Letter a", () => {
+test("testing for hello", () => {
     expect(caesarCipher("hello", 4)).toBe("lipps");
-  });
-  test("Letter b", () => {
+});
+
+test("Letter b", () => {
     expect(caesarCipher("b", 1)).toBe("c");
-  });
-  test("Letter c", () => {
+});
+
+test("testing for cat", () => {
     expect(caesarCipher("cat", 1)).toBe("dbu");
-  });
-  test("Letter d", () => {
-    expect(caesarCipher("d", 1)).toBe("e");
-  });
-  test("Letter e", () => {
-    expect(caesarCipher("e", 1)).toBe("f");
-  });
-  test("Letter f", () => {
-    expect(caesarCipher("f", 1)).toBe("g");
-  });
-  test("Letter g", () => {
-    expect(caesarCipher("g", 1)).toBe("h");
-  });
 });
